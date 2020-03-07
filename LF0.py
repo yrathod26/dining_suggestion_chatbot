6b4131849aa61fd80ab0915fdb13e011dd8a2254
Yash Rathod <55633528+yrathod26@users.noreply.github.com>
2020-03-06 23:23:44 -0500

    Update LF0.py

--- LF0.py
+++ LF0.py
@@ -4,14 +4,18 @@ import boto3
 client = boto3.client('lex-runtime')
 
 def lambda_handler(event, context):
-    print(event)
+    
     response = client.post_text(
-        botName='RestRecBot',
+        botName='RupBotTest',
         botAlias='$LATEST',
         userId='admin',
         inputText=event["message"])
-    inputText=event["message"]
+        
     return {
-        'statusCode': 200,
-        'body': response
+        "statusCode": 200,
+        "headers": {
+        'Content-Type':'application/json',    
+        'Access-Control-Allow-Origin' : '*'
+        },
+        "body": response
     }
